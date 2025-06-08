c9690ff7b243e4d02943e797453c60edc5d96184
Xavier <xaruolac@gmail.com>
2025-06-08 14:32:13 +0200

    Script classant les pays.

---
+++ autres/creer_table_superficie.py
@@ -0,0 +1,37 @@
+import pickle
+import constantes
+import os
+import pandas as pd
+
+
+# Fonction de calcul de la superficie
+def calculer_superficie(gdf, espg):
+
+    gdf = gdf.to_crs(epsg=espg)
+    gdf["superficie"] = gdf.geometry.area
+
+    gdf["superficie_par_pays"] = gdf.groupby("NAME_0")["superficie"].transform("sum")
+
+    gdf["pct_superficie_dans_pays"] = gdf["superficie"] / gdf["superficie_par_pays"]
+
+    liste_cols = list(gdf.columns)
+    liste_cols.remove("geometry")
+    liste_cols.remove("superficie_par_pays")
+
+    return gdf[liste_cols]
+
+
+# Téléchargement de la table
+with open(
+    os.path.join(constantes.direction_donnees, "carte_monde_niveau_2.pkl"),
+    "rb",
+) as f:
+    gdf = pickle.load(f)
+
+gdf = gdf.reset_index(drop=True)
+gdf_sup = calculer_superficie(gdf, espg=8857)
+
+with open(
+    os.path.join(constantes.direction_donnees, "table_superficie.pkl"), "wb"
+) as f:
+    pickle.dump(gdf_sup, f)

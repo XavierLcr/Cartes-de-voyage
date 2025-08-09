0bc0f4a93eb5d690016235bc9cc3096993c84bdc
Xavier <xaruolac@gmail.com>
2025-08-09 22:46:30 +0200

    Non utilisation d'un autre main.

--- PyQt_application.py
+++ PyQt_application.py
@@ -55,7 +55,15 @@ application_hauteur = 250
 interface_foncee = False
 inclure_emojis_onglets = True
 inclure_emojis = True
+
+# Paramètres des statistiques
 top_n_pays = None
+min_width = 500
+min_height: int = 300
+n_rangees: int = 9
+points_base: int = 15
+points_increment: int = 4
+lighter_value: int = 150
 
 ## Paramètres des cartes
 qualite_min = 200

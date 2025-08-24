93a4f78d62cd06d3e2d8e16200582192d1765fa4
Xavier <xaruolac@gmail.com>
2025-08-24 17:11:22 +0200

    Ouverture des picke dans la liste.

--- PyQt_application.py
+++ PyQt_application.py
@@ -1241,17 +1241,15 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     # Import des bases de données contenant les cartes
-    liste_gdfs = []
-    for i in range(3):
-
-        liste_gdfs.append(
-            fonctions_utiles_2_0.ouvrir_fichier(
-                direction_fichier=constantes.direction_donnees_pickle,
-                nom_fichier=f"carte_monde_niveau_{i}.pkl",
-                defaut=None,
-                afficher_erreur=f"Base de granularité {i} introuvable.",
-            )
+    liste_gdfs = [
+        fonctions_utiles_2_0.ouvrir_fichier(
+            direction_fichier=constantes.direction_donnees_pickle,
+            nom_fichier=f"carte_monde_niveau_{i}.pkl",
+            defaut=None,
+            afficher_erreur=f"Base de granularité {i} introuvable.",
         )
+        for i in range(3)
+    ]
 
     window = SettingsApp()
     window.show()

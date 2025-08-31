ad472028f537ca3fcbb89eda94a28f45211a250c
Xavier <xaruolac@gmail.com>
2025-08-31 22:58:56 +0200

    Maj avec les nouvelles directions.

--- _2_Préparation/2_2_creer_yaml_granu.py
+++ _2_Préparation/2_2_creer_yaml_granu.py
@@ -6,63 +6,15 @@ from constantes import (
     direction_donnees_autres,
     direction_donnees_application,
 )
-from _0_Utilitaires._0_1_Fonctions_utiles import ouvrir_fichier, exporter_fichier
+from _0_Utilitaires._0_1_Fonctions_utiles import ouvrir_fichier, cree_yaml_un_pays
 
 
-def cree_yaml_un_pays(
-    gdf,
-    direction_fichier,
-    nom_fichier,
-    nom_pays: None | list = ["France"],
-    granularite: int = 1,
-):
-    """Crée le yaml pour un pays à un niveau de granularité donné
+for granularite in range(1, 6):
 
-    Args:
-        gdf: la base geopandas.
-        nom_pays (str): nom du pays dans la dataframe geopandas.
-        granularite (int): 1 (faible) à 5 (forte). Sera réduite si inexistante.
-        nom (str): nom du document.
-    """
-
-    if nom_pays is not None:
-        gdf = gdf[gdf["name_0"].isin(nom_pays)]
-
-    # On garde le pays et la subdivision voulue
-    gdf = set(zip(gdf["name_0"], gdf[f"name_{granularite}"]))
-
-    # Créer un DataFrame avec les résultats
-    liste_combinaisons = (
-        pd.DataFrame(list(gdf), columns=["nom1", "nom2"])
-        # Tri par nom2
-        .sort_values(by="nom2", inplace=False)
-    )
-
-    if granularite > 1 and len(liste_combinaisons) == 1:
-        cree_yaml_un_pays(
-            gdf=gdf,
-            nom_pays=nom_pays,
-            granularite=granularite - 1,
-            direction_fichier=direction_fichier,
-            nom_fichier=nom_fichier,
-        )
-    else:
-
-        # Exporter vers YAML
-        exporter_fichier(
-            objet=liste_combinaisons.groupby("nom1")["nom2"].apply(list).to_dict(),
-            direction_fichier=direction_fichier,
-            nom_fichier=nom_fichier,
-            sort_keys=True,
-        )
-
-
-for granularite in range(6):
-
-    print(granularite, end=" ; ")
+    print(granularite)
 
     gdf = ouvrir_fichier(
-        direction_fichier=direction_donnees_autres,
+        direction_fichier=direction_donnees_application,
         nom_fichier=f"carte_monde_niveau_{granularite}.pkl",
         defaut=None,
         afficher_erreur="Base non trouvée.",
@@ -72,6 +24,6 @@ for granularite in range(6):
         gdf=gdf,
         nom_pays=None,
         granularite=granularite,
-        direction_fichier=direction_donnees_autres,
+        direction_fichier=direction_donnees_application,
         nom_fichier=f"liste_pays_granularite_{granularite}.yaml",
     )

197f5c23bfaa0abc1f9492affe966f574168e4a4
Xavier <xaruolac@gmail.com>
2025-09-06 14:32:40 +0200

    Màj script compilation.

--- _4_Interface/_4_2_compilation.py
+++ _4_Interface/_4_2_compilation.py
@@ -9,26 +9,18 @@ from cx_Freeze import setup, Executable
 import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import constantes
-
-
-# Définition de la liste des fichiers/dossiers à inclure
-include_files = [
-    r"C:\Users\xaruo\Documents\Voyages\liste_pays_departements.yaml",
-    r"C:\Users\xaruo\Documents\Voyages\liste_pays_regions.yaml",
-    r"C:\Users\xaruo\Documents\Voyages\LICENSE – MesVoyages.md",
-    (constantes.direction_donnees_application, "Données application"),
-]
-for f in os.listdir(constantes.direction_donnees_pickle):
-    if f.endswith(".pkl") and not any(chiffre in f for chiffre in ["3", "4", "5"]):
-        chemin_absolu = os.path.join(constantes.direction_donnees_pickle, f)
-        chemin_destination = f"Données pickle/{f}"
-        include_files.append((chemin_absolu, chemin_destination))
+from constantes import (
+    direction_donnees_geographiques,
+    direction_donnees_application,
+    direction_donnees_traductions,
+    direction_donnees_autres,
+    version_logiciel,
+)
 
 
 setup(
     name="MesVoyages",
-    version=constantes.version_logiciel,
+    version=version_logiciel,
     description="Créez facilement vos cartes de voyages personnalisées et suivez vos aventures autour du monde !",
     options={
         "build_exe": {
@@ -54,15 +46,40 @@ setup(
                 "json",
                 "textwrap",
             ],
-            "include_files": include_files,
+            # Liste des fichiers/dossiers à inclure
+            "include_files": (
+                # YAMLs des régions et départements
+                [
+                    os.path.join(direction_donnees_application, nom)
+                    for nom in [
+                        "liste_pays_granularite_1.yaml",
+                        "liste_pays_granularite_2.yaml",
+                    ]
+                ]
+                + [
+                    # Licence
+                    os.path.join(direction_donnees_autres, "LICENSE – MesVoyages.md"),
+                    # Paramètres, interface, ...
+                    (direction_donnees_application, "2 – Données application"),
+                    # Traductions
+                    (direction_donnees_traductions, "3 – Traductions"),
+                ]
+                # Données pickle des frontières géographiques
+                + [
+                    (
+                        os.path.join(direction_donnees_geographiques, f),
+                        os.path.join("1 – Données géographiques", f),
+                    )
+                    for f in os.listdir(direction_donnees_geographiques)
+                    if not any(chiffre in f for chiffre in "345")
+                ]
+            ),
         }
     },
     executables=[
         Executable(
             "PyQt_application_main.py",
-            icon=os.path.join(
-                constantes.direction_donnees_application, "icone_application.ico"
-            ),
+            icon=os.path.join(direction_donnees_application, "icone_application.ico"),
             target_name="MesVoyages.exe",
             base="Win32GUI",
         )

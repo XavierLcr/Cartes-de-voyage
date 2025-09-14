810cb684640d103d8aec5cd96b252067e3927da9
Xavier <xaruolac@gmail.com>
2025-09-14 20:30:36 +0200

    Initialisation script.

---
+++ _4_Interface/_4_1_Onglets/onglet_2/onglet_2_1_recommendations.py
@@ -0,0 +1,10 @@
+################################################################################
+# Projet de cartes de voyage                                                   #
+# _4_Interface/_4_1_Onglets/onglet_2                                           #
+# Onglet 2 – Suggestions de nouvelles destinations                             #
+################################################################################
+
+
+import os
+import numpy as np
+import constantes

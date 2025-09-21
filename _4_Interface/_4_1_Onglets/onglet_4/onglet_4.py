109f2ef78083fa22fce07bfd2b9e7435574386c8
Xavier <xaruolac@gmail.com>
2025-09-21 11:56:21 +0200

    Version partiellement fonctionnelle.

--- _4_Interface/_4_1_Onglets/onglet_4/onglet_4.py
+++ _4_Interface/_4_1_Onglets/onglet_4/onglet_4.py
@@ -5,6 +5,8 @@
 ################################################################################
 
 
+import copy
+
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
@@ -14,7 +16,11 @@ from PyQt6.QtWidgets import (
     QStackedWidget,
 )
 
-from _4_Interface._4_1_Onglets.onglet_4 import onglet_4_1_hemicycle, onglet_4_2_classement
+from _4_Interface._4_1_Onglets.onglet_4 import (
+    onglet_4_1_hemicycle,
+    onglet_4_2_classement,
+    onglet_4_3_recommendations,
+)
 
 
 class OngletTopPays(QWidget):
@@ -29,10 +35,11 @@ class OngletTopPays(QWidget):
 
         self.mise_en_page = constantes.parametres_application["onglet_4_mise_en_page"]
         self.fonction_traduction = fct_traduction
+        self.constantes = constantes
 
         # === Création des pages
 
-        ## === Onglet Hémicycle ===
+        ## === Onglet Hémicycle === ##
         self.hemicycle = onglet_4_1_hemicycle.HemicycleWidget(
             constantes=constantes,
         )
@@ -40,11 +47,20 @@ class OngletTopPays(QWidget):
         layout_hemicycle = QVBoxLayout(page_hemicycle)
         layout_hemicycle.addWidget(self.hemicycle)
 
-        ## === Onglet Classement ===
+        ## === Onglet Classement === ##
         self.classement_widget = onglet_4_2_classement.ClassementPays(
             constantes=constantes,
             table_superficie=table_superficie,
             fct_traduction=self.fonction_traduction,
+        )
+
+        ## === Pop-up de recommandations === ##
+        self.recommandations = onglet_4_3_recommendations.PaysAVisiter(
+            constantes=constantes,
+            table_superficie=table_superficie,
+            fct_traduire=fct_traduction,
+            top_n=20,
+            parent=None,
         )
 
         # === Mise en page ===
@@ -56,17 +72,22 @@ class OngletTopPays(QWidget):
             self.pages = QStackedWidget()
             self.pages.addWidget(page_hemicycle)  # index 0
             self.pages.addWidget(self.classement_widget)  # index 1
+            self.pages.addWidget(self.recommandations)  # index 2
 
             # === Barre de boutons (navigation) ===
             btn_layout = QHBoxLayout()
             self.btn_hemicycle = QPushButton("Hémicycle")
             self.btn_top_pays = QPushButton("Top Pays")
+            self.btn_recommandations = QPushButton("Suggestions")
             btn_layout.addWidget(self.btn_hemicycle)
             btn_layout.addWidget(self.btn_top_pays)
+            btn_layout.addWidget(self.btn_recommandations)
 
             # Connexions
             self.btn_hemicycle.clicked.connect(lambda: self.pages.setCurrentIndex(0))
             self.btn_top_pays.clicked.connect(lambda: self.pages.setCurrentIndex(1))
+            self.btn_recommandations.clicked.connect(lambda: self.pages.setCurrentIndex(2))
+            # self.btn_recommandations.clicked.connect(self.pop_up_recommandations)
 
             # Layout principal
             layout.addLayout(btn_layout)
@@ -83,12 +104,14 @@ class OngletTopPays(QWidget):
             layout.addWidget(self.sous_onglets)
 
     def set_dicts_granu(self, dict_nv):
-        self.hemicycle.set_pays_visites(pays_visites=dict_nv)
-        self.classement_widget.set_dicts_granu(dict_nv)
+        self.hemicycle.set_pays_visites(pays_visites=copy.deepcopy(dict_nv))
+        self.classement_widget.set_dicts_granu(dict_nv=copy.deepcopy(dict_nv))
+        self.recommandations.set_dicts_granu(dict_nv=copy.deepcopy(dict_nv))
 
     def set_langue(self, nouvelle_langue):
         self.hemicycle.set_langue(langue=nouvelle_langue)
         self.classement_widget.set_langue(nouvelle_langue)
+        self.recommandations.set_langue(langue=nouvelle_langue)
 
         texte_onglet_1 = self.fonction_traduction(
             "titre_sous_onglet_4_1",
@@ -98,10 +121,15 @@ class OngletTopPays(QWidget):
             "titre_sous_onglet_4_2",
             suffixe=(" 🏆"),
         )
+        texte_onglet_3 = self.fonction_traduction(
+            "titre_sous_onglet_4_3",
+            suffixe=(" 🚄​"),
+        )
 
         if self.mise_en_page == 0:
             self.btn_hemicycle.setText(texte_onglet_1)
             self.btn_top_pays.setText(texte_onglet_2)
+            self.btn_recommandations.setText(texte_onglet_3)
         elif self.mise_en_page == 1:
             self.sous_onglets.setTabText(
                 self.sous_onglets.indexOf(self.hemicycle.parentWidget()), texte_onglet_1
@@ -109,3 +137,14 @@ class OngletTopPays(QWidget):
             self.sous_onglets.setTabText(
                 self.sous_onglets.indexOf(self.classement_widget), texte_onglet_2
             )
+
+    # def pop_up_recommandations(self):
+
+    # dialogue = onglet_4_3_recommendations.PaysAVisiter(
+    #     constantes=self.constantes,
+    #     fct_traduire=self.fonction_traduction,
+    #     top_n=10,
+    #     parent=None,
+    # )
+    # dialogue.setStyleSheet(self.styleSheet())
+    # dialogue.exec()
--- _4_Interface/_4_1_Onglets/onglet_4/onglet_4_3_recommendations.py
+++ _4_Interface/_4_1_Onglets/onglet_4/onglet_4_3_recommendations.py
@@ -5,11 +5,12 @@
 ################################################################################
 
 
-import os
+import os, copy
 import numpy as np
 import pandas as pd
 from _0_Utilitaires._0_1_Fonctions_utiles import distance_haversine
 from PyQt6.QtWidgets import (
+    QWidget,
     QApplication,
     QMainWindow,
     QPushButton,
@@ -20,6 +21,7 @@ from PyQt6.QtWidgets import (
     QCheckBox,
     QDialogButtonBox,
     QStyle,
+    QScrollArea,
 )
 
 from _0_Utilitaires._0_1_Fonctions_utiles import vider_layout
@@ -33,7 +35,16 @@ def calculer_distance_deux_lieux(ligne1: pd.Series, ligne2: pd.Series, alpha=1 /
     index = [
         i
         for i in ligne1.index
-        if i not in ["name_0", "name_1", "latitude", "longitude", "superficie", "population"]
+        if i
+        not in [
+            "name_0",
+            "name_1",
+            "name_2",
+            "latitude",
+            "longitude",
+            "superficie",
+            "population",
+        ]
     ]
 
     return np.linalg.norm(ligne1[index] - ligne2[index]) / (
@@ -51,7 +62,7 @@ def calculer_distance_deux_lieux(ligne1: pd.Series, ligne2: pd.Series, alpha=1 /
 
 
 def calculer_recommandation(df: pd.DataFrame, dict_visite: dict, top_n=10):
-
+    print(dict_visite)
     dict_visite = {(p, r) for p, regions in dict_visite.items() for r in regions}
 
     df_visite = df[df.apply(lambda row: (row["name_0"], row["name_1"]) in dict_visite, axis=1)]
@@ -63,13 +74,14 @@ def calculer_recommandation(df: pd.DataFrame, dict_visite: dict, top_n=10):
         ).mean(),
         axis=1,
     )
+    print(len(df_reste[df_reste["score_region"].isna() == False]))
 
     return df_reste.sort_values("score_region", ascending=False).head(top_n)
 
 
-class PaysAVisiter(QDialog):
+class PaysAVisiter(QWidget):
 
-    def __init__(self, df_caract, constantes, fct_traduire, parent=None, top_n=10):
+    def __init__(self, constantes, table_superficie, fct_traduire, parent=None, top_n=10):
         super().__init__(parent)
 
         self.langue = "français"
@@ -77,75 +89,107 @@ class PaysAVisiter(QDialog):
         self.fonction_traduire = fct_traduire
         self.dict_granu = {"region": {}, "dep": {}}
         self.top_n = top_n
-        self.df_caracteristiques = df_caract
         self.df = None
+        self.table_superficie = table_superficie
 
         layout = QVBoxLayout()
-
         # Bouton de lancement
         self.bouton_recommandations = QPushButton()
         layout.addWidget(self.bouton_recommandations)
+        self.bouton_recommandations.clicked.connect(self.calculer_afficher_recommandation)
 
-        # Widgets post classement
-        self.titre_recommandations = QLabel()
-        self.titre_recommandations.setVisible(False)
-        layout.addWidget(self.titre_recommandations)
+        # Scroll area pour les recommandations
+        scroll_widget = QWidget()  # widget qui contiendra le layout des recommandations
+        self.corps_recommandations = QVBoxLayout()  # layout pour les QLabel
+        scroll_widget.setLayout(self.corps_recommandations)
 
-        self.corps_recommandations = QVBoxLayout()
-        layout.addLayout(self.corps_recommandations)
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)  # permet au scroll de s’adapter à la taille
+        scroll_area.setWidget(scroll_widget)
+
+        layout.addWidget(scroll_area)
+
+        self.setLayout(layout)
 
     def calculer_prochaine_destination(self):
 
-        # Mise à jour visuelle
-        self.bouton_recommandations.setVisible(False)
-        self.titre_recommandations.setVisible(True)
-
         vider_layout(self.corps_recommandations)
+
+        # Affichage temporaire
         label_temp = QLabel()
         label_temp.setPixmap(
-            self.style().standardPixmap(
-                enum_value=getattr(QStyle.StandardPixmap, "SP_BrowserReload")
-            )
+            self.style().standardPixmap(getattr(QStyle.StandardPixmap, "SP_BrowserReload"))
         )
         self.corps_recommandations.addWidget(label_temp)
 
+        # Ajout des régions des départements
+        dt_temp = self.table_superficie[
+            self.table_superficie.apply(
+                lambda row: (row["name_0"], row["name_2"])
+                in {(p, r) for p, dep in self.dict_granu.get("dep").items() for r in dep},
+                axis=1,
+            )
+        ]
+        dict_temp = copy.deepcopy(self.dict_granu.get("region"))
+        for pays, groupe in dt_temp.groupby("name_0"):
+            regions = groupe["name_1"].tolist()
+            if pays in dict_temp:
+                # Ajouter les nouvelles régions sans doublons
+                dict_temp[pays] = list(set(dict_temp[pays]) | set(regions))
+            else:
+                # Ajouter le pays s'il n'existait pas encore
+                dict_temp[pays] = regions
 
         # Calcul de la table
-        self.df = calculer_recommandation(
-            df=self.df_caracteristiques, dict_visite=self.dict_granu, top_n=self.top_n
+        self.df = (
+            calculer_recommandation(
+                df=self.constantes.df_caracteristiques_pays,
+                dict_visite=dict_temp,
+                top_n=self.top_n,
+            ).reset_index()
+            if dict_temp != {}
+            else None
         )
+
+    def afficher_recommandation(self):
 
         # Affichage
         vider_layout(self.corps_recommandations)
+        if self.df is None:
+            return
+        self.corps_recommandations.addWidget(
+            QLabel(self.fonction_traduire("titre_recommandations", suffixe=" :"))
+        )
         if self.df is not None:
             if len(self.df) > 0:
 
                 for i, ligne in self.df.iterrows():
 
                     self.corps_recommandations.addWidget(
-                    QLabel(
-                        # Numéro
-                        f"{i + 1}. "
-                        # Pays avec emoji
-                        f"<b>{ligne["name_0"]}</b> {self.constantes.emojis_pays.get(ligne["name_0"], '')}: "
-                        # Régions si affichage regroupé
-                        f"{ligne["name_1"]}",
-                        wordWrap=True,
+                        QLabel(
+                            # Numéro
+                            f"{i + 1}. "
+                            # Pays avec emoji
+                            f"<b>{ligne['name_0']}</b> {self.constantes.emojis_pays.get(ligne['name_0'], '')}: "
+                            # Régions si affichage regroupé
+                            f"{ligne['name_1']} : {ligne['score_region']}",
+                            wordWrap=True,
+                        )
                     )
-                )
-        
 
-    # def set_dicts_granu(self, dict_nv: dict):
-    #     """Permet de mettre à jour les sélections de destinations."""
-    #     self.dicts_granu = dict_nv
-    #     self.df = calculer_recommandation(
-    #         df=self.df_caracteristiques, dict_visite=self.dict_granu, top_n=self.top_n
-    #     )
+    def calculer_afficher_recommandation(self):
+        self.calculer_prochaine_destination()
+        self.afficher_recommandation()
+
+    def set_dicts_granu(self, dict_nv: dict):
+        """Permet de mettre à jour les sélections de destinations."""
+        self.dict_granu = dict_nv
+        # self.df = calculer_recommandation(
+        #     df=self.df_caracteristiques, dict_visite=self.dict_granu, top_n=self.top_n
+        # )
+        if self.dict_granu == {"region": {}, "dep": {}}:
+            self.calculer_prochaine_destination()
 
     def set_langue(self, langue: str):
         self.langue = langue
-
         self.bouton_recommandations.setText(self.fonction_traduire("bouton_recommandations"))
-        self.titre_recommandations.setText(
-            self.fonction_traduire("titre_recommandations"), suffixe=" :"
-        )

361e3e6beda258791e5fbcbb905ca48ed699281c
Xavier <xaruolac@gmail.com>
2025-09-27 00:23:53 +0200

    Modif en-tête.

--- _4_Interface/_4_1_Onglets/onglet_4/onglet_4_3_recommendations.py
+++ _4_Interface/_4_1_Onglets/onglet_4/onglet_4_3_recommendations.py
@@ -1,7 +1,7 @@
 ################################################################################
 # Projet de cartes de voyage                                                   #
-# _4_Interface/_4_1_Onglets/onglet_2                                           #
-# Onglet 2 – Suggestions de nouvelles destinations                             #
+# _4_Interface/_4_1_Onglets/onglet_4                                           #
+# Onglet 4.3 – Suggestions de nouvelles destinations                           #
 ################################################################################
 
 

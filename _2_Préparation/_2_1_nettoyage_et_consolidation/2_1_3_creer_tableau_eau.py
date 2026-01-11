2e65237e1bb52f6fd0bd0eb5e6fd2b685d31ed6a
Xavier <xaruolac@gmail.com>
2026-01-11 15:04:05 +0100

    Utilisation de la table intérmédiaire.

--- _2_Préparation/_2_1_nettoyage_et_consolidation/2_1_3_creer_tableau_eau.py
+++ _2_Préparation/_2_1_nettoyage_et_consolidation/2_1_3_creer_tableau_eau.py
@@ -6,18 +6,19 @@
 
 
 import os, sys
-import geopandas as gpd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import constantes
-from _0_Utilitaires._0_1_fonctions_utiles_gen import exporter_fichier
+from constantes import direction_donnees_intermediaires, direction_donnees_geographiques
+from _0_Utilitaires._0_1_fonctions_utiles_gen import exporter_fichier, ouvrir_fichier
 
 
 # 1 -- Import des données ------------------------------------------------------
 
-gdf = gpd.read_file(
-    os.path.join(constantes.direction_donnees, "_1_1_Données_brutes", "gadm_410.gpkg")
-).rename(columns={f"NAME_{i}": f"name_{i}" for i in range(6)})
+gdf = ouvrir_fichier(
+    direction_fichier=direction_donnees_intermediaires,
+    nom_fichier="geodataframe_propre.pkl",
+    defaut=None,
+)
 
 
 # 2 -- Sélection des lignes faisant allusion à des étendues d'eau --------------
@@ -28,13 +29,14 @@ mask = (
     .apply(lambda col: col.str.lower().isin(["water body", "waterbody"]))
     .any(axis=1)
 )
+gdf = gdf[mask]
 
 
 # 3 -- Export ------------------------------------------------------------------
 
 
 exporter_fichier(
-    objet=gdf[mask],
-    direction_fichier=constantes.direction_donnees_geographiques,
+    objet=gdf,
+    direction_fichier=direction_donnees_geographiques,
     nom_fichier="carte_monde_lacs.pkl",
 )

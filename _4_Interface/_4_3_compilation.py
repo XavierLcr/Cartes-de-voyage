9d58f35f2d313c104af95349cf1e2db2612c33a0
Xavier <xaruolac@gmail.com>
2026-02-02 23:33:59 +0100

    Mise dans un try.

--- _4_Interface/_4_3_compilation.py
+++ _4_Interface/_4_3_compilation.py
@@ -8,7 +8,8 @@
 from cx_Freeze import setup, Executable
 import os, sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.getcwd())
+
 from constantes import (
     direction_donnees_geographiques,
     direction_donnees_application,
@@ -18,80 +19,87 @@ from constantes import (
 )
 
 
-setup(
-    name="Mes Voyages",
-    version=version_logiciel,
-    description="Créez facilement vos cartes de voyages personnalisées et suivez vos aventures autour du monde !",
-    options={
-        "build_exe": {
-            "build_exe": r"C:\Users\xaruo\Documents\Voyages\Application",
-            "packages": [
-                "os",
-                "sys",
-                "gc",
-                "time",
-                "threading",
-                "warnings",
-                "copy",
-                "PyQt6",
-                "numpy",
-                "pandas",
-                "geopandas",
-                "math",
-                "random",
-                "matplotlib",
-                "colorsys",
-                "yaml",
-                "pickle",
-                "json",
-                "textwrap",
-                "numba",
-                "pygame",
-                "subprocess",
-                "platform",
-            ],
-            # Liste des fichiers/dossiers à inclure
-            "include_files": (
-                [
-                    os.path.join(direction_donnees_autres, nom)
-                    for nom in [
-                        # YAMLs des régions et départements
-                        "liste_pays_granularite_1.yaml",
-                        "liste_pays_granularite_2.yaml",
-                        # Licence
-                        "LICENSE – MesVoyages.md",
+try:
+
+    setup(
+        name="Mes Voyages",
+        version=version_logiciel,
+        description="Créez facilement vos cartes de voyages personnalisées et suivez vos aventures autour du monde !",
+        options={
+            "build_exe": {
+                "build_exe": r"C:\Users\xaruo\Documents\Voyages\Application",
+                "packages": [
+                    "os",
+                    "sys",
+                    "gc",
+                    "time",
+                    "threading",
+                    "warnings",
+                    "copy",
+                    "PyQt6",
+                    "numpy",
+                    "pandas",
+                    "geopandas",
+                    "math",
+                    "random",
+                    "matplotlib",
+                    "colorsys",
+                    "yaml",
+                    "pickle",
+                    "json",
+                    "textwrap",
+                    "numba",
+                    "pygame",
+                    "subprocess",
+                    "platform",
+                ],
+                # Liste des fichiers/dossiers à inclure
+                "include_files": (
+                    [
+                        os.path.join(direction_donnees_autres, nom)
+                        for nom in [
+                            # YAMLs des régions et départements
+                            "liste_pays_granularite_1.yaml",
+                            "liste_pays_granularite_2.yaml",
+                            # Licence
+                            "LICENSE – MesVoyages.md",
+                        ]
                     ]
-                ]
-                + [
-                    # Paramètres, interface, ...
-                    (
-                        direction_donnees_application,
-                        os.path.join("Données", "2 – Données application"),
-                    ),
-                    # Traductions
-                    (
-                        direction_donnees_traductions,
-                        os.path.join("Données", "3 – Traductions"),
-                    ),
-                ]
-                # Données pickle des frontières géographiques
-                + [
-                    (
-                        os.path.join(direction_donnees_geographiques, f),
-                        os.path.join("Données", "1 – Données géographiques", f),
-                    )
-                    for f in os.listdir(direction_donnees_geographiques)
-                    if not any(chiffre in f for chiffre in "345")
-                ]
-            ),
-        }
-    },
-    executables=[
-        Executable(
-            "PyQt_application.py",
-            icon=os.path.join(direction_donnees_application, "icone_application.ico"),
-            target_name="Mes Voyages.exe",
-            base="Win32GUI",
-        )
-    ],
-)
+                    + [
+                        # Paramètres, interface, ...
+                        (
+                            direction_donnees_application,
+                            os.path.join("Données", "2 – Données application"),
+                        ),
+                        # Traductions
+                        (
+                            direction_donnees_traductions,
+                            os.path.join("Données", "3 – Traductions"),
+                        ),
+                    ]
+                    # Données pickle des frontières géographiques
+                    + [
+                        (
+                            os.path.join(direction_donnees_geographiques, f),
+                            os.path.join("Données", "1 – Données géographiques", f),
+                        )
+                        for f in os.listdir(direction_donnees_geographiques)
+                        if not any(chiffre in f for chiffre in "345")
+                    ]
+                ),
+            }
+        },
+        executables=[
+            Executable(
+                "PyQt_application.py",
+                icon=os.path.join(
+                    direction_donnees_application, "icone_application.ico"
+                ),
+                target_name="Mes Voyages.exe",
+                base="Win32GUI",
+            )
+        ],
+    )
+
+except Exception as e:
+    print(e)

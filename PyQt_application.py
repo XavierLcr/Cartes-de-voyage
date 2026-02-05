eeaf6bd0df0bc6ae0f04dc42e1baba0012bcac3e
Xavier <xaruolac@gmail.com>
2026-02-05 23:54:14 +0100

    Correction erreur de style.

--- PyQt_application.py
+++ PyQt_application.py
@@ -130,9 +130,9 @@ class MesVoyagesApplication(QWidget):
             self.publier_cartes
         )
         # Mise à jour du style
-        self.onglet_parametres.utiliser_theme.stateChanged.connect(self.set_style)
-        self.onglet_parametres.color_combo.currentTextChanged.connect(self.set_style)
-        self.onglet_parametres.theme_combo.currentTextChanged.connect(self.set_style)
+        self.onglet_parametres.utiliser_theme.stateChanged.connect(lambda: self.set_style(theme_application=None))
+        self.onglet_parametres.color_combo.currentTextChanged.connect(lambda: self.set_style(theme_application=None))
+        self.onglet_parametres.theme_combo.currentTextChanged.connect(lambda: self.set_style(theme_application=None))
 
         # === Deuxième onglet ===
 

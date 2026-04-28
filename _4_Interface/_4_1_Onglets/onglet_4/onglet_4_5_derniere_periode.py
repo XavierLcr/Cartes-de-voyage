1b57ee1cae496fcaf7b8b6754c08e47f71f1bc0e
Xavier <xaruolac@gmail.com>
2026-04-28 13:23:14 +0200

    Fonction qui fait tout.

--- _4_Interface/_4_1_Onglets/onglet_4/onglet_4_5_derniere_periode.py
+++ _4_Interface/_4_1_Onglets/onglet_4/onglet_4_5_derniere_periode.py
@@ -25,7 +25,9 @@ from _0_Utilitaires._0_8_plot_diagramme_barres import plot_diagramme_barre
 ## 1.1 -- Fonction de transformation du dictionnaire en table générale ---------
 
 
-def creer_table_derniers_voyages(dictionnaire: dict):
+def creer_table_derniers_voyages(
+    dictionnaire: dict, date_min: str | None = None, date_max: str | None = None
+):
 
     dict_temp = dictionnaire.copy()
 
@@ -52,13 +54,34 @@ def creer_table_derniers_voyages(dictionnaire: dict):
                 }
             )
 
+    # Mise au format data.frame
     df_temp = pd.DataFrame(rows)
-    # Filtre sur les dates
+    df_temp = df_temp[~df_temp["date_debut"].isna()]
 
-    return pd.DataFrame(rows)
+    # Mise au format date
+    df_temp["date_debut"] = pd.to_datetime(df_temp["date_debut"])
+    df_temp["date_fin"] = pd.to_datetime(df_temp["date_fin"])
 
+    # Ajout d'une colonne avec une liste de date
+    df_temp["mois"] = df_temp.apply(
+        lambda row: pd.date_range(
+            start=row["date_debut"],
+            end=row["date_fin"],
+            freq="MS",  # début de chaque mois
+        ),
+        axis=1,
+    )
 
-## 1.2 -- Création de la table finale ------------------------------------------
+    # Expansion de la table
+    df_temp = df_temp.explode("mois").reset_index(drop=True)
+
+    # Filtres sur les dates
+    if date_min is not None:
+        df_temp = df_temp[df_temp["mois"] >= pd.to_datetime(date_min)]
+    if date_max is not None:
+        df_temp = df_temp[df_temp["mois"] <= pd.to_datetime(date_max)]
+
+    return df_temp
 
 
 # 2 -- Classe PyQt6 ------------------------------------------------------------

392164f78582c3c843e0ffee520e2935ecbe2f9e
Xavier <xaruolac@gmail.com>
2026-05-24 15:47:46 +0200

    Utilisation de nouvelles fonctions pour la granularité, les régions et le suivi.

--- _4_Interface/_4_1_Onglets/onglet_1/onglet_1_1_creation_cartes.py
+++ _4_Interface/_4_1_Onglets/onglet_1/onglet_1_1_creation_cartes.py
@@ -6,10 +6,13 @@
 
 
 from PyQt6.QtCore import pyqtSignal, QObject
-from _3_Calculs._3_4_carte_main import cree_graphe_depuis_debut
+from _3_Calculs._3_4_carte_main import (
+    cree_graphe_depuis_debut,
+    creer_multiples_cartes,
+    lister_cartes_a_publier,
+)
 from _0_Utilitaires._0_1_fonctions_utiles_gen import (
     voyages_vers_destinations_une_granu,
-    charger_gdfs,
 )
 
 # 1 -- Classe de suivi de l'avancement de la publication des cartes ------------
@@ -35,15 +38,32 @@ class CreerCartes(QObject):
 
         super().__init__()
 
+        # Récupération des paramètres
         self.parametres = params
         self.constantes = constantes
 
-    def run(self):
-
-        # === Ajustement des variables ===
-        self.granularite = {"Pays": 0, "Région": 1, "Département": 2}.get(
-            self.parametres.get("granularite"), -1
+        # Liste des cartes à publier
+        self.liste_cartes = lister_cartes_a_publier(
+            regroupements_pays_ref=constantes.liste_pays_groupes,
+            continents_ref=constantes.liste_regions_monde,
+            traductions_ref=constantes.pays_differentes_langues,
+            langue=self.parametres["langue"],
+            pays=self.parametres["cartes_des_pays"],
+            monde=self.parametres["carte_du_monde"],
+            continents=self.recuperer_continents(),
+            voyages=self.parametres.get("dictionnaire_voyages", {}),
+            sortir_cartes_granu_inf=self.parametres.get(
+                "sortir_cartes_granu_inf", False
+            ),
+            granularite_objectif=self.recuperer_granularite(clef="granularite"),
         )
+
+    def recuperer_granularite(self, clef: str):
+        return {"Pays": 0, "Région": 1, "Département": 2}.get(
+            self.parametres.get(clef), -1
+        )
+
+    def recuperer_continents(self):
 
         dict_bouton_continent = {
             "afrique": ["Africa"],
@@ -53,15 +73,18 @@ class CreerCartes(QObject):
             "moyen_orient": ["Middle East"],
         }
 
-        self.liste_regions_temp = {
+        # Ajout des continents souhaités
+        liste_regions_temp = {
             region: self.constantes.liste_regions_monde[region]
             for cle_param, regions in dict_bouton_continent.items()
             if self.parametres.get(cle_param, False)
             for region in regions
         }
 
+        # Ajout des régions complémentaires (si souhaité)
         if self.parametres.get("autres_regions", False):
-            self.liste_regions_temp.update(
+
+            liste_regions_temp.update(
                 {
                     k: v
                     for k, v in self.constantes.liste_regions_monde.items()
@@ -73,6 +96,13 @@ class CreerCartes(QObject):
                     ]
                 }
             )
+
+        # Renvoi
+        return liste_regions_temp
+
+    def run(self):
+
+        # === Ajustement des variables ===
 
         dict_regions = voyages_vers_destinations_une_granu(
             dict_voyages=self.parametres.get("dictionnaire_voyages", {}), clef="region"
@@ -92,12 +122,7 @@ class CreerCartes(QObject):
         if dict_regions == {}:
             dict_regions = None
 
-        self.nb_graphes.emit(
-            self.calculer_nb_total_graphes(
-                dict_regions=dict_regions,
-                dict_departement=dict_deps,
-            )
-        )
+        self.nb_graphes.emit(self.calculer_nb_total_graphes())
 
         # Gestion de l'e-mail
         if not self.parametres.get("adresse_email") or not self.parametres.get(
@@ -119,10 +144,8 @@ class CreerCartes(QObject):
             direction_resultat=self.parametres["dossier_stockage"],
             ouvrir_direction_resultat=self.parametres["ouvrir_dossier_stockage"],
             langue=self.parametres["langue"],
-            granularite_visite=self.granularite,
-            granularite_reste={"Pays": 0, "Région": 1}.get(
-                self.parametres.get("granularite_fond"), 2
-            ),
+            granularite_visite=self.recuperer_granularite(clef="granularite"),
+            granularite_reste=self.recuperer_granularite(clef="granularite_fond"),
             theme=self.constantes.liste_ambiances[
                 self.parametres.get("theme", "Pastel")
             ],
@@ -138,7 +161,7 @@ class CreerCartes(QObject):
             format=self.parametres["format"],
             qualite=self.parametres["qualite"],
             carte_du_monde=self.parametres["carte_du_monde"],
-            liste_regions=self.liste_regions_temp,
+            liste_regions=self.recuperer_continents(),
             pays_individuel=self.parametres["cartes_des_pays"],
             limite_n_cartes=self.parametres["limite_n_cartes"],
             sortir_cartes_granu_inf=self.parametres["sortir_cartes_granu_inf"],
@@ -150,22 +173,5 @@ class CreerCartes(QObject):
 
         self.finished.emit()
 
-    def calculer_nb_total_graphes(self, dict_regions, dict_departement):
-
-        return (
-            (
-                (len(list(dict_regions.keys())) if dict_regions is not None else 0)
-                * int(
-                    (self.parametres["sortir_cartes_granu_inf"] or self.granularite < 2)
-                )
-                + (
-                    len(list(dict_departement.keys()))
-                    if dict_departement is not None
-                    else 0
-                )
-            )
-            * self.parametres["cartes_des_pays"]
-            * int(self.granularite != 0)
-            + len(self.liste_regions_temp)
-            + int(self.parametres["carte_du_monde"])
-        )
+    def calculer_nb_total_graphes(self):
+        return len(self.liste_cartes.keys())

02de0b8f80ca47e9fa86abf7cec022e17b13a27a
Xavier <xaruolac@gmail.com>
2026-06-15 12:17:25 +0200

    Ajout périmètre.

--- _2_Préparation/_2_1_nettoyage_et_consolidation/2_1_4_creer_table_superficie.py
+++ _2_Préparation/_2_1_nettoyage_et_consolidation/2_1_4_creer_table_superficie.py
@@ -12,15 +12,14 @@ import os, sys
 
 sys.path.append(os.getcwd())
 
-import constantes
+from constantes import direction_donnees_geographiques, direction_donnees_application
 from _0_Utilitaires._0_1_fonctions_utiles_gen import ouvrir_fichier, exporter_fichier
-
 
 # 1 -- Import des données ------------------------------------------------------
 
 
 gdf = ouvrir_fichier(
-    direction_fichier=constantes.direction_donnees_geographiques,
+    direction_fichier=direction_donnees_geographiques,
     nom_fichier="carte_monde_niveau_2.pkl",
     defaut=None,
 ).reset_index(drop=True)
@@ -35,13 +34,17 @@ def calculer_superficie(gdf, epsg):
         gdf.copy()
         .to_crs(epsg=epsg)
         .assign(
+            # Statistiques par niveau le plus fin
             superficie=lambda x: x.geometry.area,
+            perimetre=lambda x: x.geometry.length,
+            # Statistiques nationales
             superficie_par_pays=lambda x: x.groupby("name_0")["superficie"].transform(
                 "sum"
             ),
             pct_superficie_dans_pays=lambda x: x["superficie"]
             / x["superficie_par_pays"],
         )
+        # Suppression de colonnes inutiles
         .drop(columns=["geometry", "superficie_par_pays"])
     )
 
@@ -57,6 +60,6 @@ gdf = calculer_superficie(gdf=gdf, epsg=8857)
 
 exporter_fichier(
     objet=gdf,
-    direction_fichier=constantes.direction_donnees_application,
+    direction_fichier=direction_donnees_application,
     nom_fichier="table_superficie.pkl",
 )
